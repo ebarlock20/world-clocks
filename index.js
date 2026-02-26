803f9d0eda925a4d7ca85037137e257dae89fd4d
ebarlock20 <ebarlock20@gmail.com>
2026-02-26 17:07:41 -0500

    Added JS code

---
+++ index.js
@@ -0,0 +1,27 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format("h:mm:ss [<small>]A[</small>]")}`;
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = `${newYorkTime.format("h:mm:ss [<small>]A[</small>]")}`;
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = `${tokyoTime.format("h:mm:ss [<small>]A[</small>]")}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
